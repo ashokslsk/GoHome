aec8f34b00a2635997eb51adb6096927885e5af6
Ashok kumar <ashok.kumar@ashoks-MacBook-Pro-2.local>
2017-08-05 16:53:45 +0530

    Added vector maths logic

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -4,38 +4,61 @@ using UnityEngine;
 
 public class ConsolePrinter : MonoBehaviour {
 
+	public Vector2 location;
+	public Vector2 homeLocation; // Distance in meters.
+
+
 	// Use this for initialization
 	void Start () {
 
-		float Location = 5.0f; // Distance in meters.
-		float homeLocation = 2.1f; // Distance in meters.
-		float distnace = homeLocation - Location;
 
 		print ("Hello World, Welcome to Go Home");
 		print ("I will finish the Go home chapter before \n 3 oclock today");
 
-		print ("Distance : " + distnace);
 
-		if (Location > homeLocation) {
-			
-			print ("Go Back to get home");
-		} 
-
-		if(Location < homeLocation){
-			print ("Go forward to get home");
-		}
-
-		if (Location == homeLocation) {
-			print ("I am at home ");
-		}
 	}
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			print ("Left key pressed");
+			location = location + new Vector2 (-1.0f, 0);
+
+			Vector2 pathToHome = homeLocation - location;
+			print ("Distance to home : " + pathToHome.magnitude);
+
+			if (location == homeLocation) {
+				print ("I am at home ");
+			}
 		}
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			print ("Right key pressed");
+
+			location = location + new Vector2 (1.0f, 0);
+			Vector2 pathToHome = homeLocation - location;
+			print ("Distance to home : " + pathToHome.magnitude);
+
+			if (location == homeLocation) {
+				print ("I am at home ");
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			location = location + new Vector2 (0, 1);
+			Vector2 pathToHome = homeLocation - location;
+			print ("Distance to home : " + pathToHome.magnitude);
+
+			if (location == homeLocation) {
+				print ("I am at home ");
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+
+			location = location + new Vector2 (0, -1);
+			Vector2 pathToHome = homeLocation - location;
+			print ("Distance to home : " + pathToHome.magnitude);
+
+			if (location == homeLocation) {
+				print ("I am at home ");
+			}
+
 		}
 	}
 }
